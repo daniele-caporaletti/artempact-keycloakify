505a61d46180179df0373c6a8e5a1c8f3c452e99
daniele-caporaletti <danielecaporaletti.01@gmail.com>
2025-03-27 14:12:41 +0100

    remove default stylesheet

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -3,7 +3,12 @@ import type { ClassKey } from "keycloakify/login";
 import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
-import Template from "keycloakify/login/Template";
+import Template from "./Template";
+import Login from "./pages/Login";
+import LoginResetPassword from "./pages/LoginResetPassword";
+import LoginVerifyEmail from "./pages/LoginVerifyEmail";
+import LogoutConfirm from "./pages/LogoutConfirm";
+import Register from "./pages/Register";
 const UserProfileFormFields = lazy(
     () => import("./UserProfileFormFields")
 );
@@ -21,6 +26,48 @@ export default function KcPage(props: { kcContext: KcContext }) {
         <Suspense>
             {(() => {
                 switch (kcContext.pageId) {
+                    case "login.ftl":
+                        return (
+                            <Login
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                            />
+                        );
+                    case "login-reset-password.ftl":
+                        return (
+                            <LoginResetPassword
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                            />
+                        );
+                    case "login-verify-email.ftl":
+                        return (
+                            <LoginVerifyEmail
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                            />
+                        );
+                    case "logout-confirm.ftl":
+                        return (
+                            <LogoutConfirm
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                            />
+                        );
+                    case "register.ftl":
+                        return (
+                            <Register
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                                UserProfileFormFields={UserProfileFormFields}
+                                doMakeUserConfirmPassword={true}
+                            />
+                        );
                     default:
                         return (
                             <DefaultPage
